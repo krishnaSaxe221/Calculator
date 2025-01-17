ebd7bad86ba76286c4c1d8dd8c776b9fe114a09b
krishna saxena <100754752+krishnaSaxe221@users.noreply.github.com>
2025-01-17 14:48:12 +0530

    Added: Perform subtraction of 3 numbers

---
+++ src/Subtraction.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Subtraction {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the first number:");
+        double num1 = scanner.nextDouble();
+
+        System.out.println("Enter the second number:");
+        double num2 = scanner.nextDouble();
+
+        System.out.println("Enter the third number:");
+        double num3 = scanner.nextDouble();
+        System.out.println(num1 - num2 - num3);
+    }
+}
